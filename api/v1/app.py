b1e81457277f79a03e12b900787e019b062b0b57
jdcera4 <jdcera4@gmail.com>
2021-09-18 11:17:05 -0400

    correct pep8

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
 """creating an api"""
+from os import getenv
 from flask import Flask
 from api.v1.views import app_views
 from models import storage
-from os import getenv
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def restart_data(exception):
     """close session so the new data is outputed"""
     storage.close()
+
 
 if __name__ == "__main__":
     app.run(
